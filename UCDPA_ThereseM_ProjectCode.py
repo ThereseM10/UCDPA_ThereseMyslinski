ed3bd83899f7cf22076b879927632ffd46b549f8
ThereseM10 <theresemyslinski@gmail.com>
2021-06-21 13:42:18 +0100

    Added Dataset 3 analysis to UCDPA_ThereseM_ProjectCode.py

--- UCDPA_ThereseM_ProjectCode.py
+++ UCDPA_ThereseM_ProjectCode.py
@@ -99,3 +99,141 @@ dc_marvel_merge = dc_pivot.merge(marvel_pivot, how='left', left_index=True,
                                  right_on=['Year', 'SEX'], suffixes=['_dc', '_marvel'])
 print(dc_marvel_merge)
 
+
+# Dataset 3: NBA Players
+# 1) Importing Data
+# Objective: Importing CSV
+# Importing CSV and saving as pandas DataFrame with missing values recognised as blank spaces.
+NBA_import = pd.read_csv('all_seasons.csv', na_values=' ')
+
+# Using head() function to see first five rows of the data to ensure it imported correctly.
+print(NBA_import.head())
+
+# Using the drop() function to delete the first column as it does not contribute to the dataset.
+# The axis argument is set to 1, this is to delete the whole column.
+NBA_import.drop('Unnamed: 0', inplace=True, axis=1)
+
+# 2) Analysing Data: NBA Height and Weight for Numpy objective
+# Objective: Missing Values / Drop Duplicates
+# Replacing missing values with NaN using the replace() function.
+# The first argument is what missing values were set to be recognised as when importing and the second argument is changing them to NaN.
+NBA_replace = NBA_import.replace(' ', np.nan)
+
+# Objective: Grouping
+# Using a pivot table to calculate the total number of players in each season.
+# The values are sorted using the sort_values() function by the season with the highest number of players.
+print(NBA_replace.pivot_table(values="player_name", index="season", aggfunc='count').sort_values('player_name', ascending=False))
+
+# Objective: Slicing (loc / iloc)
+# The index is set to be the 'season' column to make it easier to subset.
+# To slice the index, it must be sorted first. This was done using the sort_index() function.
+NBA_ind = NBA_replace.set_index('season').sort_index()
+
+# The 2017-18 season is shown to have the most number of players.
+# Slicing for 2017-18 season and the height and weight columns of NBA players.
+# The height and weight columns are renamed using the rename() function and columns argument to allow for easier column access.
+NBA_height_weight = NBA_ind.loc['2017-18', ['player_height', 'player_weight']].rename(columns={'player_height': 'height',
+                                                                                                      'player_weight': 'weight'})
+print(NBA_height_weight)
+
+# Objective: Numpy
+# Using the numpy array function, the height and weight columns are converted into a 2D Numpy array.
+height_weight_np = np.array(NBA_height_weight[['height', 'weight']])
+print(type(height_weight_np))
+
+# Using Numpy's mean and median functions, the average height and weight of the players are calcualted and printed.
+# Both mean and median were used in case the mean was affected by outliers.
+mean_height = np.mean(height_weight_np[:,0])
+mean_weight = np.mean(height_weight_np[:,1])
+median_height = np.median(height_weight_np[:, 0])
+median_weight = np.median(height_weight_np[:, 1])
+# The mean and median calculations are converted to string as can only print likeable data types.
+print("Mean height: " + str(mean_height) + "; Mean weight: " + str(mean_weight))
+print("Median height: " + str(median_height) + "; Median weight: " + str(median_weight))
+
+# For later data visualisation analysis, the height column was subset into another numpy array and divided by 100.
+height_cm_np = np.array(NBA_height_weight['height'])
+height_m_np = height_cm_np / 100
+
+# Using the Numpy min() function to find the height of the smallest player.
+print(np.min(height_m_np))
+
+# Using Boolean operator 'and' to find how many players are smaller than the average.
+print(height_m_np[np.logical_and(height_m_np > 1, height_m_np < 2)])
+
+# Preparing height and weight data for the visualization objective.
+# Slicing for the 2017-18 season rows and the players' name, points they got, and height and weight columns.
+# The index was reset and then deleted to allow for easier plotting.
+NBA_seventeen_season = NBA_ind.loc['2017-18', ['player_name', 'pts',
+                                               'player_height',
+                                               'player_weight',]].reset_index().drop(columns='season', axis=1)
+
+# Using the pandas.Series() function, the previously calculated height in meters column replaces the height (cm) column.
+NBA_seventeen_season['player_height'] = pd.Series(height_m_np)
+print(NBA_seventeen_season)
+
+# Using the replace() function and column argument, the height and weight columns are renamed to allow for ease of access.
+NBA_plot = NBA_seventeen_season.rename(columns={'player_height': 'Height (m)', 'player_weight': 'Weight (kg)'})
+print(NBA_plot)
+
+# 3) Visualise
+# Objective: Seaborn / Matplotlib
+# Using Seaborn relplot() function to plot a scatter plot.
+sns.set()
+# The background style is set to darkgrid to make it easier to read the plotted data.
+sns.set_style("darkgrid")
+# The relplot() function is used with kind argument set to scatter.
+# The x and y axis are set to show height and weight, and the data argument equals the data wish to plot.
+# The plot is assigned to the variable 'g' as this is common practice when using Seaborn plots.
+g = sns.relplot(x='Height (m)', y='Weight (kg)', data=NBA_plot, kind='scatter')
+# As this plot is a FacetGrid, the fig.suptitle function is used to add a title.
+# The y argument sets the height of the title and the fontsize argument is to manually adjust the fontsize.
+g.fig.suptitle("Height and Weight of NBA Players in the 2017-18 season", y=1, fontsize=14)
+g.set(xlabel='Height (m)', ylabel='Weight (cm)')
+plt.show()
+
+# 2) Analysing Data: NBA Players' Countries of Origin for data visualization objective.
+# Objective: Looping, iterrows()
+# Using For Loop on the NBA Player dataset to get the player's names who scored more than the average number of points.
+NBA_most_points = NBA_seventeen_season.set_index(["player_name"])
+for i, row in NBA_most_points.iterrows():
+    if row['pts'] > NBA_most_points['pts'].median():
+        print(i + ": " + str(row['pts']) + " points")
+
+# Objective: Slicing
+# Setting the index to country for slicing.
+NBA_index = NBA_import.set_index('country').sort_index()
+print(NBA_index)
+
+# Slicing for all the rows and columns that is USA and saving to a new variable.
+# Using the reset_index() function to make it easier to drop the USA column.
+NBA_USA = NBA_index.loc[['USA'], :].reset_index()
+
+# Removing USA rows and saving to a new variable.
+NBA_drop = NBA_index.drop(labels='USA', axis=0).reset_index()
+
+# Subsetting only country and season columns for data visualization objective.
+NBA_other = NBA_drop[["country", "season"]]
+
+# Objective: Grouping
+# Using the groupby() and unique() functions to calculate the number of countries in each season.
+# Resetted the index to allow for plotting.
+NBA_plot = NBA_other.groupby('season')['country'].nunique().to_frame().reset_index()
+print(NBA_plot)
+
+# 3) Visualize
+# Objective: Seaborn / Matplotlib
+# Setting the scale
+sns.set()
+# Setting the context to paper as the plot will be inserted into a report document.
+sns.set_context("paper")
+# Using set() function to manually set the font size.
+sns.set(font_scale=1)
+# Using catplot to show the number of countries present in each season, excluding USA.
+g = sns.catplot(x='season', y='country', data=NBA_plot, kind='bar')
+# Setting FacetGrid title and axis labels
+g.fig.suptitle("Number of Countries (excluding USA) in each NBA Season (1996 to 2020)", y=1)
+g.set(xlabel="NBA Season", ylabel="Total Number of Countries")
+# Adjusting tick rotation to make the x-axis labels easier to read.
+plt.xticks(rotation=30)
+plt.show()
